beb435f3fb6aa7b691937a740b118d654c67e39e
mako-agawa <mako.agawa@gmail.com>
2024-08-30 16:07:27 +0900

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get "sessions/new"
+  # Root path to index action
+  root "articles#index"
+
+  # CRUD routes without using resources
+  get    'users',          to: 'users#index',   as: 'users'
+  get    'signup',      to: 'users#new',     as: 'signup'
+  post   'users',          to: 'users#create'
+  get    'users/:id',      to: 'users#show',    as: 'user'
+  get    'users/:id/edit', to: 'users#edit',    as: 'edit_user'
+  patch  'users/:id',      to: 'users#update'
+  put    'users/:id',      to: 'users#update'
+  delete 'users/:id',      to: 'users#destroy'
+
+  get    "/login",   to: "sessions#new",     as: 'login'
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+
+
+  resources :articles
+
+end
+
---
+++ db/migrate/20240830014650_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
